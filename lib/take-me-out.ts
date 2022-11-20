07f164994a45dccd797d83136f379c4d00380526
Guntur Poetra <poetra.guntur@gmail.com>
2022-11-20 19:13:33 +0800

    :boom:

--- lib/take-me-out.ts
+++ lib/take-me-out.ts
@@ -1,57 +1,57 @@
 function splitNumbers(n: number) {
-	return n
-		.toString()
-		.split('')
-		.map((v) => parseInt(v, 10));
+  return n
+    .toString()
+    .split("")
+    .map((v) => parseInt(v, 10));
 }
 
 function sum(numbers: number[]) {
-	const value = numbers.reduce((a, b) => a + b, 0);
+  const value = numbers.reduce((a, b) => a + b, 0);
 
-	return value;
+  return value;
 }
 
 function deepSum(numbers: number[]) {
-	let value = sum(numbers);
+  let value = sum(numbers);
 
-	if (value > 9) {
-		const numberArray = splitNumbers(value);
-		value = deepSum(numberArray);
-	}
+  if (value > 9) {
+    const numberArray = splitNumbers(value);
+    value = deepSum(numberArray);
+  }
 
-	return value;
+  return value;
 }
 
 function nameMapNumber(input: string) {
-	const letters = input.split('').filter((s) => s !== '' && s !== ' ');
-	const codeMap: number[] = [];
+  const letters = input.split("").filter((s) => s !== "" && s !== " ");
+  const codeMap: number[] = [];
 
-	for (const l of letters) {
-		codeMap.push(l.charCodeAt(0));
-	}
+  for (const l of letters) {
+    codeMap.push(l.charCodeAt(0));
+  }
 
-	return codeMap;
+  return codeMap;
 }
 
 export function count(name: string, partnerName: string) {
-	const nameMap = nameMapNumber(name);
-	const partnerNameMap = nameMapNumber(partnerName);
-	const numbersMap = [...nameMap, ...partnerNameMap];
-	const firstMap = [];
-	const lastMap = [];
+  const nameMap = nameMapNumber(name);
+  const partnerNameMap = nameMapNumber(partnerName);
+  const numbersMap = [...nameMap, ...partnerNameMap];
+  const firstMap = [];
+  const lastMap = [];
 
-	for (const n of numbersMap) {
-		const f = parseInt(n.toString().charAt(0), 10);
-		const l = parseInt(n.toString().charAt(1), 10);
+  for (const n of numbersMap) {
+    const f = parseInt(n.toString().charAt(0), 10);
+    const l = parseInt(n.toString().charAt(1), 10);
 
-		firstMap.push(f);
-		lastMap.push(l);
-	}
+    firstMap.push(f);
+    lastMap.push(l);
+  }
 
-	const summary1 = deepSum(firstMap);
-	const summary2 = deepSum(lastMap);
+  const summary1 = deepSum(firstMap);
+  const summary2 = deepSum(lastMap);
 
-	const result = [summary1, summary2].join('');
+  const result = [summary1, summary2].join("");
 
-	return parseInt(result, 10);
+  return parseInt(result, 10);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,140 +1,183 @@
-import Head from 'next/head';
-import {useEffect, useState} from 'react';
-import {count} from '../lib/take-me-out';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { count } from "../lib/take-me-out";
+import styles from "../styles/Home.module.css";
 
 const InputBlockComponent = {
-	marginBottom: '0.5rem',
+  marginBottom: "0.5rem",
 };
 
 const InputTextElement = {
-	fontWeight: 700,
-	fontSize: '2.5rem',
-	padding: '0.8rem 1rem',
-	textAlign: 'center',
-	background: '#faf0f9',
-	color: '#111111',
-	fontVariant: 'all-small-caps',
-	borderRadius: '5px',
-	boxShadow: 'none',
-	border: 'none',
-	width: '100%',
+  fontWeight: 700,
+  fontSize: "2.5rem",
+  padding: "0.8rem 1rem",
+  background: "#faf0f9",
+  color: "#111111",
+  fontVariant: "all-small-caps",
+  borderRadius: "5px",
+  boxShadow: "none",
+  border: "none",
+  width: "100%",
 };
 
 const ButtonElement = {
-	height: '2.5rem',
-	width: '100%',
-	background: '#ff27b2',
-	borderRadius: '5px',
-	boxShadow: 'none',
-	border: 'none',
-	fontSize: '1rem',
-	fontWeight: 700,
-	color: '#FFFFFF',
-	cursor: 'pointer',
+  height: "2.5rem",
+  width: "100%",
+  background: "#ff27b2",
+  borderRadius: "5px",
+  boxShadow: "none",
+  border: "none",
+  fontSize: "1rem",
+  fontWeight: 700,
+  color: "#FFFFFF",
+  cursor: "pointer",
 };
 
+function randomPercentage() {
+  const value = Math.random().toString().slice(2, 4);
+  return parseInt(value, 10);
+}
+
 export default function Home() {
-	const [name, setName] = useState('');
-	const [partnerName, setPartnerName] = useState('');
-	const [result, setResult] = useState(0);
-	const [isReady, setReady] = useState(false);
+  const [name, setName] = useState("");
+  const [partnerName, setPartnerName] = useState("");
+  const [result, setResult] = useState(0);
+  const [isReady, setReady] = useState(false);
 
-	const handleOnChange = (key: 'name' | 'partnerName') => (e: any) => {
-		let value: string = e?.target?.value ?? '';
+  const handleOnChange = (key: "name" | "partnerName") => (e: any) => {
+    let value: string = e?.target?.value ?? "";
 
-		const isValid = /^[a-zA-Z\s]+$/gi.test(value);
+    const isValid = /^[a-zA-Z\s]+$/gi.test(value);
 
-		if (value.length > 0 && !isValid) {
-			return;
-		}
+    if (value.length > 0 && !isValid) {
+      return;
+    }
 
-		// value = value.toUpperCase();
-		value = value.toLowerCase();
-		// console.log({key, value, isValid});
+    value = value.toLowerCase();
 
-		if (key === 'name') {
-			setName(value);
-		} else {
-			setPartnerName(value);
-		}
-	};
+    if (key === "name") {
+      setName(value);
+    } else {
+      setPartnerName(value);
+    }
+  };
 
-	useEffect(() => {
-		setReady(false);
-		const result = count(name, partnerName) || 0;
+  function showResult() {
+    const result = count(name, partnerName) || 0;
 
-		if (name.trim().length > 0 && partnerName.trim().length > 0) {
-			setReady(true);
+    if (name.trim().length > 0 && partnerName.trim().length > 0) {
+      setReady(true);
+      setResult(result);
+    } else {
+      setReady(false);
+      setResult(0);
+    }
+  }
 
-			setResult(result);
-		}
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setReady(true);
 
-		return () => {
-			return;
-		};
-	}, [name, partnerName]);
+    const idn = setInterval(() => {
+      setResult(randomPercentage());
+    }, 100);
 
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Take Me Out</title>
-				<meta name='description' content='Take Me Out' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+    setTimeout(() => {
+      clearInterval(idn);
+      showResult();
+    }, 2000);
+  };
 
-			<main className={styles.main}>
-				<div style={{width: '600px'}}>
-					<div style={{marginBottom: `5rem`}}>
-						<div style={{marginBottom: '1.5rem'}}>
-							<div style={InputBlockComponent}>
-								<span>
-									<input
-										style={InputTextElement}
-										type='text'
-										placeholder='Nama kamu...'
-										value={name}
-										onChange={handleOnChange('name')}
-									/>
-								</span>
-							</div>
+  // useEffect(() => {
+  // 	setReady(false);
+  // 	showResult();
 
-							<div style={InputBlockComponent}>
-								<h1 className={styles.title}>
-									<span>{'💘'}</span>
-								</h1>
-							</div>
+  // 	return () => {
+  // 		return;
+  // 	};
+  // }, [name, partnerName]);
 
-							<div style={InputBlockComponent}>
-								<span>
-									<input
-										style={InputTextElement}
-										type='text'
-										placeholder='Nama gebetan / pasangan kamu...'
-										value={partnerName}
-										onChange={handleOnChange('partnerName')}
-									/>
-								</span>
-							</div>
-						</div>
+  return (
+    <>
+      <style jsx global>{`
+        html,
+        body {
+          background-color: black;
+          font-family: "Helvetica Neue";
+        }
+      `}</style>
 
-						{/*
-						<div>
-							<button style={ButtonElement}>GO!!!</button>
-						</div>
-						 */}
-					</div>
+      <div className={styles.container}>
+        <Head>
+          <title>Take Me Out</title>
+          <meta name="description" content="Take Me Out" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-					{isReady ? (
-						<div>
-							<h1 className={styles.title}>
-								<span>{result}</span>
-								<span>%</span>
-							</h1>
-						</div>
-					) : null}
-				</div>
-			</main>
-		</div>
-	);
+        <main className={styles.main}>
+          <div style={{ width: "600px" }}>
+            <div style={{ marginBottom: `5rem` }}>
+              <div style={{ marginBottom: "1.5rem" }}>
+                <div style={InputBlockComponent}>
+                  <span>
+                    <input
+                      style={{
+                        ...InputTextElement,
+                        textAlign: "center",
+                      }}
+                      type="text"
+                      placeholder="Nama kamu..."
+                      value={name}
+                      onChange={handleOnChange("name")}
+                    />
+                  </span>
+                </div>
+
+                <div style={InputBlockComponent}>
+                  <h1 className={styles.title}>
+                    <span>💘</span>
+                  </h1>
+                </div>
+
+                <div style={InputBlockComponent}>
+                  <span>
+                    <input
+                      style={{
+                        ...InputTextElement,
+                        textAlign: "center",
+                      }}
+                      type="text"
+                      placeholder="Nama gebetan / pasangan kamu..."
+                      value={partnerName}
+                      onChange={handleOnChange("partnerName")}
+                    />
+                  </span>
+                </div>
+              </div>
+
+              <div>
+                <button style={ButtonElement} onClick={handleSubmit}>
+                  TAKE ME OUT
+                </button>
+              </div>
+            </div>
+
+            {isReady ? (
+              <div>
+                <h1
+                  className={styles.title}
+                  style={{
+                    color: "#FFFFFF",
+                  }}
+                >
+                  <span>{result}</span>
+                  <span>%</span>
+                </h1>
+              </div>
+            ) : null}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
